2beaa075c7f5c10a195f3ad76c08d046bb5b0c9c
Alexander <aozertsov@gmail.com>
2020-04-25 17:40:57 +0300

    codesmell fixes

--- handler/CandleHandler.py
+++ handler/CandleHandler.py
@@ -10,7 +10,7 @@ class CandleHandler(Handler):
 
     def __init__(self, storage: Storage):
         super(CandleHandler, self).__init__([21016])
-        self._storage = CandleStorage("/home/alexander/Documents/arqaconnector/data")
+        self._storage = storage
 
     def _handle(self, json_msg):
         graphs = json_msg['graph']
--- storage/KafkaStorage.py
+++ storage/KafkaStorage.py
@@ -6,7 +6,7 @@ from kafka import KafkaProducer
 from storage.Storage import Storage
 
 
-class KafkaStorage(Storage):
+class KafkaStorage(Storage, ABC):
 
     # WTF, why exception, when pass servers??? TODO
     def __init__(self, bootstrap_servers = 'localhost:9092', key_serializer = str.encode, value_serializer = lambda v: json.dumps(v).encode('utf-8')):
