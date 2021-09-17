ad448e98069a72d609550e5343f0ccdfd2293847
aozertsov <aozertsov@gmail.com>
2021-09-17 21:54:09 +0300

    fix codestyle

--- storage/KafkaStorage.py
+++ storage/KafkaStorage.py
@@ -9,7 +9,10 @@ from storage.Storage import Storage
 class KafkaStorage(Storage, ABC):
 
     # WTF, why exception, when pass servers??? TODO
-    def __init__(self, bootstrap_servers = 'localhost:9092', key_serializer = str.encode, value_serializer = lambda v: json.dumps(v).encode('utf-8')):
-        self._producer = KafkaProducer(bootstrap_servers = 'localhost:9092',
-                                        key_serializer=key_serializer,
-                                        value_serializer= value_serializer)
+    def __init__(self,
+                 bootstrap_servers='localhost:9092',
+                 key_serializer=str.encode,
+                 value_serializer=lambda v: json.dumps(v).encode('utf-8')):
+        self._producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                                       key_serializer=key_serializer,
+                                       value_serializer=value_serializer)
