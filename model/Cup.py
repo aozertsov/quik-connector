8d5410e90b403a02603433e080707c43220adfa6
aozertsov <aozertsov@gmail.com>
2021-10-28 16:13:03 +0300

    update cup class repr

--- model/Cup.py
+++ model/Cup.py
@@ -6,10 +6,9 @@ class Cup:
         asks = list(filter(lambda x: int(x[1]['s']) > 0, lines.items()))
         self._buys = list(map(lambda x: CupElement('buy', x[0], x[1]['b']), buys))
         self._sells = list(map(lambda x: CupElement('sell', x[0], x[1]['s']), asks))
-        a = 10
 
     def __str__(self):
-        return f'{{"timestamp": {self._timestamp}, "buys": {self._buys}, "sells": {self._sells} }}'
+        return f'{{"timestamp": "{self._timestamp}", "buys": {self._buys}, "sells": {self._sells} }}'
 
 
 class CupElement:
@@ -20,7 +19,7 @@ class CupElement:
         self._price = price
 
     def __str__(self):
-        return f'{{"type": {self._type}, "count": {self._count}, "price": {self._price} }}'
+        return f'{{"type": "{self._type}", "count": {self._count}, "price": {self._price} }}'
 
     def __repr__(self):
-        return f'{{"type": {self._type}, "count": {self._count}, "price": {self._price} }}'
+        return f'{{"type": "{self._type}", "count": {self._count}, "price": {self._price} }}'
