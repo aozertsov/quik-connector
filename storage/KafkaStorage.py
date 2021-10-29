1126ff839963952d5b173c41d269cb9499451319
aozertsov <aozertsov@gmail.com>
2021-10-29 21:46:33 +0300

    use parameter bootstrap_server

--- storage/KafkaStorage.py
+++ storage/KafkaStorage.py
@@ -8,11 +8,10 @@ from storage.Storage import Storage
 
 class KafkaStorage(Storage, ABC):
 
-    # WTF, why exception, when pass servers??? TODO
     def __init__(self,
                  bootstrap_servers='localhost:9092',
                  key_serializer=str.encode,
                  value_serializer=lambda v: json.dumps(v).encode('utf-8')):
-        self._producer = KafkaProducer(bootstrap_servers='localhost:9092',
+        self._producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
                                        key_serializer=key_serializer,
                                        value_serializer=value_serializer)
